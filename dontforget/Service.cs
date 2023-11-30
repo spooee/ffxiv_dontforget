48d100ae45f09dba853ffa527735b6df97093553
spooee <32413258+spooee@users.noreply.github.com>
2023-11-30 13:43:43 +0100

    Update Service.cs

--- dontforget/Service.cs
+++ dontforget/Service.cs
@@ -7,19 +7,8 @@ namespace dontforget
 {
     internal class Service
     {
-        [PluginService] public static IPluginLog Log { get; private set; } = null!;
-        [PluginService] public static ICommandManager CommandManager { get; private set; } = null!;
-        [PluginService] public static IDataManager DataManager { get; private set; } = null!;
-        [PluginService] public static IObjectTable ObjectTable { get; private set; } = null!;
-        [PluginService] public static ITargetManager TargetManager { get; private set; } = null!;
         [PluginService] public static IClientState ClientState { get; private set; } = null!;
-        [PluginService] public static ISigScanner SigScanner { get; private set; } = null!;
-        [PluginService] public static IGameInteropProvider Hook { get; private set; } = null!;
         [PluginService] public static ICondition Condition { get; private set; } = null!;
-        [PluginService] public static IGameGui GameGui { get; private set; } = null!;
-        [PluginService] public static IChatGui ChatGui { get; private set; } = null!;
-        [PluginService] public static ITextureProvider TextureProvider { get; private set; } = null!;
-        [PluginService] public static IAddonLifecycle AddonLifecycle { get; private set; } = null!;
         [PluginService] public static IFramework Framework { get; private set; } = null!;
         [PluginService] public static IDutyState DutyState { get; private set; } = null!;
     }
